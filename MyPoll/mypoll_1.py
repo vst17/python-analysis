6890b642fbb80701c92f97d63222fc4b7911ed46
Veronika Tueros <vst17@rutgers.edu>
2018-05-30 09:09:12 -0400

    updated files

--- MyPoll/mypoll_1.py
+++ MyPoll/mypoll_1.py
@@ -1,3 +1,4 @@
+
 # import operating systems and csv file
 import os
 import csv
@@ -20,7 +21,7 @@ with open(csvpath, newline = "") as csvfile:
     # total vote count; finding individual vote counts
     for row in csvreader:
         total_votes +=1
-        if row[2] in candidates.key():
+        if row[2] in candidates.keys():
             candidates[row[2]] += 1
         else:
             candidates[row[2]] = 1
@@ -36,4 +37,34 @@ for key in candidates.keys():
         winner_count = candidates[key]
 
 # tests
-print(total_votes)
+#print(total_votes)
+# tests
+# print(total_vote_count)
+# print(candidates)
+# print(candidates_percent)
+# print(winner)
+
+# printing to the terminal
+print("Election Results")
+print("-------------------------------------")
+print("Total Votes: " + str(total_votes))
+print("-------------------------------------")
+for key, value in candidates.items():
+    print(key + ": " + str(candidates_percent[key]) + "% (" + str(value) + ")")
+print("-------------------------------------")
+print("Winner: " + winner)
+print("-------------------------------------")
+
+# creating new text file
+new_file = open("Output/results_1.txt", "w")
+
+# writing the new file
+new_file.write("Election Results \n")
+new_file.write("------------------------------------- \n")
+new_file.write("Total Votes: " + str(total_votes) + "\n")
+new_file.write("------------------------------------- \n")
+for key, value in candidates.items():
+    new_file.write(key + ": " + str(candidates_percent[key]) + "% (" + str(value) + ") \n")
+new_file.write("------------------------------------- \n")
+new_file.write("Winner: " + winner + "\n")
+new_file.write("------------------------------------- \n")#
